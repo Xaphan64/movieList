42b761121c0432742ac0e126f784ad47c017e211
Xaphan64 <alex_daniel64@yahoo.com>
2026-04-18 17:17:06 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-white text-3xl">Tailwind works 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
